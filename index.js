b15d1a3559e4a0560ae4a70045e1daefd47d5e02
LikithRoshanReddyKankanala <146204708+LikithRoshanReddyKankanala@users.noreply.github.com>
2025-03-18 13:11:28 +0000

    add changes

--- index.js
+++ index.js
@@ -10,18 +10,17 @@ const taskModal = document.querySelector(".task__modal__body");
 //console.lod(taskModal);
 
 const htmlTaskContent = ({id, title, description, type, url }) => {};
-  <div class="col-md-6 col-lg-4 mt-3" id=${id} >
+  <div class="col-md-6 col-lg-4 mt-3" key='${id}' >
     <div class='card shadow-sm task__card'>
+
       <div class='card-header d-flex justify-content-end task__card__header'>
-         <button type='button' class='btn btn-outline-info mr-1.5' name=${id}> 
+         <button type='button' class='btn btn-outline-info mr-1.5' name= '${id}'> 
               <i class='fas fa-pencil-alt name=${id}'></i>
          </button>
-         <button type='button' class='btn btn-outline-danger mr-1.5' name=${id}> 
+         <button type='button' class='btn btn-outline-danger mr-1.5' name='${id}'> 
               <i class='fas fa-trash-alt name=${id}'></i>
          </button>
       </div>
-    </div>
 
-    <div class='card'>
     </div>
-  </div>
+    </div>

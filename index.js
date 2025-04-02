e64fa327ff7e57b31c61eb5a2b7245b681895333
LikithRoshanReddyKankanala <146204708+LikithRoshanReddyKankanala@users.noreply.github.com>
2025-04-02 08:44:18 +0000

    day-6 done

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-// const state = {
-  taskList: []
+const state = {
+  taskList: [],
 };
 
 //DOM Opertaions
@@ -16,10 +16,10 @@ const htmlTaskContent = ({id, title, description, type, url }) =>
     <div class='card shadow-sm task__card'>
 
       <div class='card-header d-flex justify-content-end task__card__header'>
-         <button type='button' class='btn btn-outline-info mr-1.5' name= ${id}> 
+         <button type='button' class='btn btn-outline-info mr-1.5' name= '${id}' onclick="editTask.apply(this, arguments)"> 
               <i class='fas fa-pencil-alt name=${id}'></i>
          </button>
-         <button type='button' class='btn btn-outline-danger mr-1.5' name=${id} onclick="deletetask.apply(this, arguments)"> 
+         <button type='button' class='btn btn-outline-danger mr-1.5' name='${id}' onclick="deletetask.apply(this, arguments)"> 
               <i class='fas fa-trash-alt name=${id}'></i>
          </button>
       </div>
@@ -28,8 +28,8 @@ const htmlTaskContent = ({id, title, description, type, url }) =>
           // url &&
           // <img width='100%' src=${url} alt='Card Image' class='card-img-top md-3 rounded-lg' />
           url
-          ?<img width='100%' src=${url} alt='Card Image' class='card-img-top md-3 rounded-lg' />
-          :<img width='100%' src=${ /workspaces/Tasky-application/collection3.jpg}  alt='Card Image' class='card-img-top md-3 rounded-lg' />
+          ?<img width='100%' src="${url}" alt='Card Image' class='card-img-top md-3 rounded-lg' />
+          :<img width='100%' src="https://plus.unsplash.com/premium_photo-1686777542997-b48271320758?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dGFraW5nJTIwcGhvdG98ZW58MHx8MHx8fDA%3D" alt='Card Image' class='card-img-top md-3 rounded-lg' />
         }
         <h4 class='card=title task__card__title'>${title}</h4>
         <p class='description trim-3-lines text-muted'>${description}</p>
@@ -38,7 +38,7 @@ const htmlTaskContent = ({id, title, description, type, url }) =>
       </div>
     </div>
     <div class='card-footer'>
-          <button type='button' class='btn btn-outline-primary float-right' data-bs-toggle="modal" data-bs-target="#showTask" onclick='opentask.apply(this, arguments)' id=${id}>Open Task</button>
+          <button type='button' class='btn btn-outline-primary float-right' data-bs-toggle="modal" data-bs-target="#showTask" onclick='opentask.apply(this, arguments)' id='${id}'>Open Task</button>
     </div>
   </div>
 </div>  
@@ -47,20 +47,20 @@ const htmlTaskContent = ({id, title, description, type, url }) =>
 // Modal Body on >> clk of Open Task
 const htmlModalContent = ({id, title, description, url }) => {
   const date = new Date(parseInt(id));
-  return 
-  <div id=${id}>
+  return `
+  <div id='${id}'>
   ${
     // url &&
     // <img width='100%' src=${url} alt='Card Image' class='img-fluid place__holder__image mb-3' />
     url
-    ? <img width='100%' src=${url} alt='Card Image' class='card-img-top md-3 rounded-lg'  />
+    ? <img width='100%' src='${url}' alt='Card Image' class='card-img-top md-3 rounded-lg'  />
     : <img width='100%' src="https://tse1.mm.bing.net/th?id=OIP.F00dCf4bXxX0J-qEEf4qIQHaD6&pid=Api&rs=1&c=1&qlt=95&w=223&h=117" alt='Card Image' class='card-img-top md-3 rounded-lg' />
   }
   <strong class='text-muted text-sm'>Created on: ${date.toDateString()}</strong>
   <h2 class='my-3'>${title}</h2>
   <p class='text-muted'>${description}</P>
   </div>
-  ;
+ ` ;
 };
 
 const updateLocalstorage = () => {
@@ -95,7 +95,7 @@ VM214:2 1742719784682 --> always you get a new id. So, we get store the date as 
 
 const handleSubmit = (event) => {
   //console.log("event triggered")
-  const id = ${date.now()};
+  const id = `${date.now()}`;
   const input = {
     url: document.getElementById("imageUrl").value,
     title: document.getElementById("taskTitle").value,
@@ -147,5 +147,4 @@ const deletetask = (e) => {
 //edit task
 const editTask = (e) => {
   if (!e) e - window.event;
-  
 }
